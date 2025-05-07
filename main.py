c1301e8e76e71bc16d7fa0a0f69ce12ec4c4a154
allard-gh <a.r.snijder@gmail.com>
2025-05-07 22:58:01 +0200

    poging 1 - te moeilijk denken

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-uber_black = 2.0
-uber_van = 3.5
-uber_x = 1.5
+uber = {'black' : 2, 'van' : 3.5, 'x' : 1.5}
 
+def get_user_choice():
+    validate = True
+    while validate:
+        choice = input('Welke Uber wil je pakken? (black/van/x) \n')
+        if choice not in uber:
+            print('Onjuiste keuze\n')
+        else:
+            validate = False
+            return choice
+
+# def distance():
+#     while validate = True
+#         km = int(input('Hoeveel km wil je rijden? \n'))
+#         if type(km) == 'float':
+#             print('Voer een juist aantal kilometers in \n')
+#         else:
+#             validate = False
+#             return km
+
+def calculate_costs(choice, km):
+    ritprijs = round(uber[choice] * km, 2)
+    return ritprijs
+
+choice = get_user_choice()
+# km = distance()
+km = float(input('Hoeveel km wil je rijden? '))
+ritprijs = calculate_costs(choice, km)
+
+print(f'{km} km rijden met Uber {choice} kost €{ritprijs:.2f}')
